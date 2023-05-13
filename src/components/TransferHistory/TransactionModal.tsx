67268f3e0e576ab8f6706707b39fee938c49a2db
Luke Fiji <luke.fiji@gmail.com>
2023-05-12 18:28:09 -0700

    Create TransactionModal

--- src/components/TransferHistory/TransactionModal.tsx
+++ src/components/TransferHistory/TransactionModal.tsx
@@ -1,27 +1,52 @@
-import { Box, Modal, Typography } from '@mui/material';
+import { Box, Modal, Stack, Typography } from '@mui/material';
+import { Transfer } from '@prisma/client';
 
-interface Props {}
+interface Props {
+  selectedTransaction: Transfer | null;
+  setSelectedTransaction: (transaction: Transfer | null) => void;
+}
 
-const TransactionModal = ({}: Props) => {
-  const open = true;
-  const handleClose = () => {};
-  const style = {};
+const modalStyles = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  // width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+const TransactionModal = ({
+  selectedTransaction,
+  setSelectedTransaction,
+}: Props) => {
+  const transaction = selectedTransaction;
 
   return (
     <Modal
-      open={open}
-      onClose={handleClose}
+      open={!!selectedTransaction}
+      onClose={() => setSelectedTransaction(null)}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
-      <Box sx={style}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          Text in a modal
-        </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-        </Typography>
-      </Box>
+      {!!transaction ? (
+        <Box sx={modalStyles}>
+          <Stack spacing={2}>
+            <Typography variant="h6" component="h2">
+              Transaction Details
+            </Typography>
+            <Typography>To: {transaction.to}</Typography>
+            <Typography>Amount: {transaction.amount} SOL</Typography>
+            <Typography>Date: {transaction.createdAt.toString()}</Typography>
+            <Typography>Block: {transaction.block}</Typography>
+            <Typography>Signature: {transaction.signature}</Typography>
+          </Stack>
+        </Box>
+      ) : (
+        <Box sx={modalStyles}>Error loading transaction</Box>
+      )}
     </Modal>
   );
 };
--- src/components/TransferHistory/index.tsx
+++ src/components/TransferHistory/index.tsx
@@ -9,7 +9,10 @@ import {
   TableHead,
   TableRow,
 } from '@mui/material';
+import { Transfer } from '@prisma/client';
 import { useWallet } from '@solana/wallet-adapter-react';
+import { useState } from 'react';
+import TransactionModal from './TransactionModal';
 
 const trimTextStyles = (width: number = 150) => ({
   maxWidth: width,
@@ -24,6 +27,9 @@ const TransferHistory = ({}: Props) => {
   const { data, isLoading } = useTransferHistoryQuery(publicKey);
   const transferHistory = data ?? [];
 
+  const [selectedTransaction, setSelectedTransaction] =
+    useState<Transfer | null>(null);
+
   if (!publicKey) {
     return null;
     // <Box>
@@ -31,58 +37,78 @@ const TransferHistory = ({}: Props) => {
     // </Box>
   }
 
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ width: '100%' }} aria-label="SOL transfer history">
-        <TableHead>
-          <TableRow>
-            <TableCell>From</TableCell>
-            <TableCell>To</TableCell>
-            <TableCell align="right">Amount</TableCell>
-            <TableCell align="right">Block</TableCell>
-            <TableCell>Signature</TableCell>
-            <TableCell align="right">Date</TableCell>
-          </TableRow>
-        </TableHead>
+  console.log(selectedTransaction);
 
-        <TableBody>
-          {isLoading ? (
-            <TableRow
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell
-                component="th"
-                colSpan={6}
-                scope="row"
-                sx={{ verticalAlign: 'center', textAlign: 'center', py: 8 }}
-              >
-                <CircularProgress size={48} />
-              </TableCell>
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ width: '100%' }} aria-label="SOL transfer history">
+          <TableHead>
+            <TableRow>
+              <TableCell>From</TableCell>
+              <TableCell>To</TableCell>
+              <TableCell align="right">Amount</TableCell>
+              <TableCell align="right">Block</TableCell>
+              <TableCell>Signature</TableCell>
+              <TableCell align="right">Date</TableCell>
             </TableRow>
-          ) : (
-            transferHistory.map((row) => (
+          </TableHead>
+
+          <TableBody>
+            {isLoading ? (
               <TableRow
-                key={row.id}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
-                <TableCell component="th" scope="row" sx={trimTextStyles(100)}>
-                  {row.from}
-                </TableCell>
-                <TableCell align="right" sx={trimTextStyles(100)}>
-                  {row.to}
-                </TableCell>
-                <TableCell align="right">{row.amount} SOL</TableCell>
-                <TableCell align="right">{row.block}</TableCell>
-                <TableCell sx={trimTextStyles(150)}>{row.signature}</TableCell>
-                <TableCell align="right">
-                  {new Date(row.createdAt).toLocaleDateString('en-US')}
+                <TableCell
+                  component="th"
+                  colSpan={6}
+                  scope="row"
+                  sx={{ verticalAlign: 'center', textAlign: 'center', py: 8 }}
+                >
+                  <CircularProgress size={48} />
                 </TableCell>
               </TableRow>
-            ))
-          )}
-        </TableBody>
-      </Table>
-    </TableContainer>
+            ) : (
+              transferHistory.map((transaction) => (
+                <TableRow
+                  key={transaction.id}
+                  sx={{
+                    '&:last-child td, &:last-child th': { border: 0 },
+                    cursor: 'pointer',
+                  }}
+                  onClick={() => setSelectedTransaction(transaction)}
+                >
+                  <TableCell
+                    component="th"
+                    scope="row"
+                    sx={trimTextStyles(100)}
+                  >
+                    {transaction.from}
+                  </TableCell>
+                  <TableCell align="right" sx={trimTextStyles(100)}>
+                    {transaction.to}
+                  </TableCell>
+                  <TableCell align="right">{transaction.amount} SOL</TableCell>
+                  <TableCell align="right">{transaction.block}</TableCell>
+                  <TableCell sx={trimTextStyles(150)}>
+                    {transaction.signature}
+                  </TableCell>
+                  <TableCell align="right">
+                    {new Date(transaction.createdAt).toLocaleDateString(
+                      'en-US'
+                    )}
+                  </TableCell>
+                </TableRow>
+              ))
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TransactionModal
+        selectedTransaction={selectedTransaction}
+        setSelectedTransaction={setSelectedTransaction}
+      />
+    </>
   );
 };
 
