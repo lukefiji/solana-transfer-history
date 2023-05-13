e8a0be5389e75a5ffc4c6952eb2c2ee515006bd9
Luke Fiji <luke.fiji@gmail.com>
2023-05-12 18:09:19 -0700

    Run prisma build

---
+++ src/components/TransferHistory/TransactionModal.tsx
@@ -0,0 +1,29 @@
+import { Box, Modal, Typography } from '@mui/material';
+
+interface Props {}
+
+const TransactionModal = ({}: Props) => {
+  const open = true;
+  const handleClose = () => {};
+  const style = {};
+
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Text in a modal
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+        </Typography>
+      </Box>
+    </Modal>
+  );
+};
+
+export default TransactionModal;
